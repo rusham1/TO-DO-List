4850a156801fe619bb02effc44549e6b13ab2122
rusham1 <rusasayami@gmail.com>
2025-11-21 18:11:52 +0545

    completed

--- program.py
+++ program.py
@@ -1,3 +1,8 @@
+import os
+
+# This gives the full path of taskslist.txt in the same folder as the .py file
+FILE_PATH = os.path.join(os.path.dirname(__file__), "taskslist.txt")
+
 def showMenu():
     print("\n\n1.   View tasks.")
     print("2.   Add tasks.")
@@ -8,7 +13,7 @@ def showMenu():
 
 def loadTasks():
     try:
-        with open('taskslist.txt','r') as f:
+        with open(FILE_PATH,'r') as f:
             tasks=[line.strip() for line in f.readlines()]
     except FileNotFoundError:
         tasks=[]
@@ -16,17 +21,17 @@ def loadTasks():
 
 def saveNewTask(t):
     try:
-        with open('taskslist.txt','a') as f:
+        with open(FILE_PATH,'a') as f:
             f.write(t+'\n')
             print("content added")
     except FileNotFoundError:
-        with open('taskslist.txt','w') as f:
+        with open(FILE_PATH,'w') as f:
             f.write(t+'\n')
             print("file created and content added")
 
 def saveTasks(tasks):
     
-    with open('taskslist.txt','w') as f:
+    with open(FILE_PATH,'w') as f:
         for task in tasks:
             f.write(task+'\n')
 
@@ -37,9 +42,11 @@ def main():
         showMenu()
         choice=input("\n\nEnter your choice:    ")
         task=loadTasks()
+
         if choice=='6':
             print("\nExited.")
             break
+
         elif choice=='1':
             if not task:
                 print("Empty File")
@@ -52,6 +59,7 @@ def main():
         elif choice=='2':
             newtask=input("\nEnter the new task to add:\t")
             saveNewTask(newtask)
+
         elif choice=='3':
             if not task:
                 print("\nEmpty File.")
@@ -65,6 +73,25 @@ def main():
                     print(f"The value \"{removedItem}\" is removed.")
                 else:
                     print("Invalid index number. ")
+        
+        elif choice=='4':
+            if not task:
+                print("\nEmpty File.")
+            else:
+                for i,taskValue in enumerate(task,start=1):
+                    print(f'{i}. {taskValue}')
+                index=int(input("\nEnter the index of the task to update:\t"))-1
+                newValue=input("Enter the new task:\t")
+                task[index]=newValue
+                saveTasks(task)
+                print("Task updated")
+        
+        elif choice=='5':
+            task=[]
+            saveTasks(task)
+
+        else:
+            print("Invalid Choice. Try again.")
 
 if __name__=="__main__":
     main()
